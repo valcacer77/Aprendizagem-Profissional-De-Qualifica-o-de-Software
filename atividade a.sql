25b273fcc43f59de977739c594aea63b7a36b5bc
valcacer77 <gabrielvalcacertorquato@gmail.com>
2024-06-26 17:11:50 -0300

    Update atividade a.sql

--- atividade a.sql
+++ atividade a.sql
@@ -62,3 +62,22 @@ codigo varchar(30),
 descricao varchar(30),
 valor varchar(30)
 );
+insert into cliente(nome,cpf,endereco,telefone) 
+values
+('Gabriel Valcacer Torquato','88877766655','Taguatinga, Rua 6, Bairro Vila Dimas','61988887777')
+insert into compra(codigo, quantidade, produtos)
+values
+('2342', '2', '6');
+insert into funcionario(codigo, nome, cpf, telefone)
+values
+('3323', 'Miranda Damaseno Dos Santos', '33388899989', '61987876767')
+
+insert into departamento(codigo, descricao)
+values
+('7876', 'calcados')
+insert into fornecedor(codigo, nome, telefone)
+values
+('6655', 'Heitor Figueira Da Silva', '61988776655')
+insert into produto(codigo, descricao, valor)
+values
+('1212', 'pizzaria', '32')
